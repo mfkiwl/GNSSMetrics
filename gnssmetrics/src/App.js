1146e3e3a006a061b149aa6b50a8b55b000e3a9a
Adedolapo Adegboye <deepee019@gmail.com>
2024-04-14 19:01:37 -0600

    Project init

--- gnssmetrics/src/App.js
+++ gnssmetrics/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import "./App.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Routes, Route } from "react-router-dom";
+import Error404 from "./Dev/Contexts/Error";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Error404>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Error404 />} />
+        </Route>
+      </Routes>
+    </Error404>
   );
 }
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
 
 export default App;
