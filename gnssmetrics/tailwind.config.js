0a0391eafc465f5638f0707b2697fa95f4cbe659
Adedolapo Adegboye <deepee019@gmail.com>
2024-04-14 19:25:23 -0600

    Project init

--- gnssmetrics/tailwind.config.js
+++ gnssmetrics/tailwind.config.js
@@ -1,6 +1,7 @@
+//tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".src/**/**/*.{html,js,jsx}"],
+  content: ["./src/**/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
